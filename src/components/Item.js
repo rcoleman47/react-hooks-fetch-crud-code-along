29071cd25e682afd4524b198df9191d5b1a42e09
ryanadams <rcoleman.adams@gmail.com>
2022-05-02 18:38:32 -0500

    commit tree aa1c52e06413bbfa7b7ae257d8c8ab489b73db34

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,14 +1,39 @@
 import React from "react";
 
-function Item({ item }) {
+function Item({ item, onUpdateItem, onDeleteItem }) {
+
+  function handleDeleteClick() {
+    fetch(`http://localhost:4000/items/${item.id}`, {
+      method: "DELETE",
+    })
+      .then((r) => r.json())
+      .then(() => onDeleteItem(item));
+  }
+
+  function handleAddToCartClick() {
+    // add fetch request
+    fetch(`http://localhost:4000/items/${item.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        isInCart: !item.isInCart,
+      }),
+    })
+      .then((r) => r.json())
+      .then((updatedItem) => onUpdateItem(updatedItem));
+  }
+
   return (
     <li className={item.isInCart ? "in-cart" : ""}>
       <span>{item.name}</span>
       <span className="category">{item.category}</span>
-      <button className={item.isInCart ? "remove" : "add"}>
+      <button className={item.isInCart ? "remove" : "add"}
+      onClick={handleAddToCartClick}>
         {item.isInCart ? "Remove From" : "Add to"} Cart
       </button>
-      <button className="remove">Delete</button>
+      <button onClick={handleDeleteClick} className="remove">Delete</button>
     </li>
   );
 }
